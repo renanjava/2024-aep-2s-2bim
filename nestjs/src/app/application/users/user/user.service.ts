f95f9f8a083c1d574694e025044346c2b9cc7061
renanjava <renancvr777@gmail.com>
2024-10-08 23:07:41 -0300

    fix: user - adaptando a service para a repository

--- nestjs/src/app/application/users/user/user.service.ts
+++ nestjs/src/app/application/users/user/user.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserRepository } from './user.repository';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(protected readonly userRepository: UserRepository) { }
+
+  async create(createUserDto: CreateUserDto) {
+    await this.userRepository.create(createUserDto as User);
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    return await this.userRepository.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    return await this.userRepository.findById(id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return await this.userRepository.updateById(id, updateUserDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    return await this.userRepository.deleteById(id);
   }
 }
