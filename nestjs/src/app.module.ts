f1b9789650b1ed3d028c0106415430765ff7bd4f
Renan <renancvr777@gmail.com>
2024-10-11 17:53:51 -0300

    add: cache na versão 2 da controller de user

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './app/application/users/modules/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://0.0.0.0:27017/aep-teste2'),
     UserModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.register({
+      isGlobal: true,
+    }),
   ],
   controllers: [],
   providers: [],
