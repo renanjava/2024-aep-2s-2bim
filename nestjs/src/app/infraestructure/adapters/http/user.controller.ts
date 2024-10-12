6a017ec1e2ac219a07a4d33cc691a4092069d98d
renanjava <renancvr777@gmail.com>
2024-10-12 16:09:43 -0300

    add: hash na senha com salt

--- nestjs/src/app/infraestructure/adapters/http/user.controller.ts
+++ nestjs/src/app/infraestructure/adapters/http/user.controller.ts
@@ -1,3 +1,4 @@
+import { CacheInterceptor } from '@nestjs/cache-manager';
 import {
   Controller,
   Get,
@@ -6,26 +7,36 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
 } from '@nestjs/common';
 import { UserService } from 'src/app/application/users/services/user.service';
 import { CreateUserDto } from 'src/app/interfaces/dto/create-user.dto';
 import { UpdateUserDto } from 'src/app/interfaces/dto/update-user.dto';
+import { HashPasswordPipe } from 'src/common/pipes/hash-password.pipe';
 
 @Controller({ path: 'users', version: '1' })
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    return await this.userService.create(createUserDto);
+  async create(
+    @Body() { password, ...createUserDto }: CreateUserDto,
+    @Body('password', HashPasswordPipe) hashedPassword: string,
+  ) {
+    return await this.userService.create({
+      ...createUserDto,
+      password: hashedPassword,
+    } as CreateUserDto);
   }
 
   @Get()
+  @UseInterceptors(CacheInterceptor)
   async findAll() {
     return await this.userService.findAll();
   }
 
   @Get(':id')
+  @UseInterceptors(CacheInterceptor)
   async findOne(@Param('id') id: string) {
     return await this.userService.findOne(id);
   }
