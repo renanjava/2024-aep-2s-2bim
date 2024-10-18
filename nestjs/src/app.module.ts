57adf2ace71cfe440519dde3d8fae2e39fa57f2a
renanjava <renancvr777@gmail.com>
2024-10-18 18:07:04 -0300

    generate resource messages

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -8,6 +8,7 @@ import { AuthModule } from './common/auth/auth.module';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { GlobalLoggerInterceptor } from './common/interceptors/global-logger.interceptor';
+import { MessagesModule } from './app/messages/messages.module';
 
 @Module({
   imports: [
@@ -27,6 +28,7 @@ import { GlobalLoggerInterceptor } from './common/interceptors/global-logger.int
       },
       isGlobal: true,
     }),
+    MessagesModule,
   ],
   controllers: [],
   providers: [
